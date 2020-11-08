4895a479b33198ee59c03d5bb16873527f5bad1d
steamedbao <wanruiray@hotmail.com>
2020-11-09 07:25:42 +0800

    chat ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,35 @@
 <template>
   <section class="section">
     <div class="columns">
-      <div class="column is-2 box main-content"></div>
       <div class="column is-8">
         <Team v-for="(team, key) of teams" :key="key" :title="'Group ' + key">
           <p v-for="(member, ckey) of team.members" :key="ckey">{{ member }}</p>
         </Team>
       </div>
 
-      <div v-if="this.$store.state.userInfo.name" class="column is-2 box">
-        <li v-for="(message, key) of messages" :key="key">
+      <div
+        v-if="this.$store.state.userInfo.name"
+        class="column is-2 box"
+        title="Chat"
+        style="
+          height: 500px;
+          width: 300px;
+          position: absolute;
+          bottom: 80px;
+          right: 50px;
+          padding: 0px;
+        "
+      >
+        <header class="window__header__container">
+          <slot name="header"> Chat </slot>
+        </header>
+        <p v-for="(message, key) of messages" :key="key" style="padding: 8px">
           {{ message.user + ' : ' + message.text }}
-        </li>
-        <div class="field has-addons">
+        </p>
+        <div
+          class="field has-addons"
+          style="position: absolute; width: 280px; bottom: 5px; right: 8px"
+        >
           <div class="control">
             <input
               class="input"
@@ -22,13 +39,35 @@
             />
           </div>
           <div class="control">
-            <a class="button is-info" @click="sendMessage"> Send </a>
+            <a
+              class="button is-info"
+              @click="sendMessage"
+              style="background: #6b63d8"
+            >
+              Send
+            </a>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<style lang="scss">
+.window {
+  &__header {
+    &__container {
+      background: linear-gradient(90deg, #6b63d8, #b284ed);
+      padding: 10px;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      color: #faf9ff;
+      font-weight: 600;
+      z-index: 1;
+    }
+  }
+}
+</style>
 
 <script>
 import Team from '~/components/Team'
