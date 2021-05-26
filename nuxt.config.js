31795d55d90a13239efb9693779fa1ff0ff2a021
CaedenZ <fengye16@ichat.sp.edu.sg>
2021-05-26 22:01:08 +0800

    nxconfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        http_equiv: 'Content-Security-Policy',
+        content: 'upgrade-insecure-requests',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
